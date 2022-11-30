2bb681f8ac52cd2e4de931600d056f1c1bd1d67d
HP EliteBook <keresztesg74@gmail.com>
2022-11-30 12:50:34 +0100

    frissítés 11.30

--- beadando_feladat.py
+++ beadando_feladat.py
@@ -1,90 +1,90 @@
 import os
 from time import sleep
-print("A képernyő 4 másodperc múlva frissülni fog!")
-sleep(4)
+print("A képernyő 3 másodperc múlva frissülni fog!")
+sleep(3)
 os.system('cls')
 
 global f
 f = 0
 
 
-def city():
+def varos():
     print("Üdvözlöm a cinema city jegyfogaló oldalán! ")
     print("Melyik városba szeretne jegyet foglalni?:")
     print("1: Pécs")
     print("2: Székesfehérvár ")
     print("3: Szeged")
-    place = int(input("Válassza ki a várost: "))
-    if place == 1:
-        center()
-    elif place == 2:
-        center()
-    elif place == 3:
-        center()
+    hely = int(input("Válassza ki a várost: "))
+    if hely == 1:
+        mozivalaszto()
+    elif hely == 2:
+        mozivalaszto()
+    elif hely == 3:
+        mozivalaszto()
     else:
         print("Hibás választás")
 
-def center():
+def mozivalaszto():
     print("Melyik moziban szeretné megtekinteni? ")
     print("1: Cinema city Pécs")
     print("2: Cinema city Székesfehérvár")
     print("3: Cinema city Szeged")
     print("4: Vissza a város választóhoz!")
     a = int(input("Válassza ki a mozit: "))
-    movie(a)
+    film(a)
     return 0
-def movie(cinema):
-    if cinema == 1:
-        t_movie()
-    elif cinema == 2:
-        t_movie()
-    elif cinema == 3:
-        t_movie()
-    elif cinema == 4:
-        city()
+def film(mozi):
+    if mozi == 1:
+        t_film()
+    elif mozi == 2:
+        t_film()
+    elif mozi == 3:
+        t_film()
+    elif mozi == 4:
+        varos()
     else:
         print("Hibás választás")
 
 
-def t_movie():
+def t_film():
     global f
-    f = f+ 1
+    f = f + 1
     print("Melyik filmet szeretné megnézni?")
     print("1: Beugró a paradicsomba")
     print("2: Fekete párduc 2")
     print("3: Rosszul vagyok magamtól")
     print("4: Vissza a moziválasztóhoz!")
-    movie = int(input("Válassza ki a filmet: "))
-    if movie == 4:
-        center()
-        cinema()
+    film = int(input("Válassza ki a filmet: "))
+    if film == 4:
+        mozivalaszto()
+        mozi()
         return 0
     if f == 1:
-        cinema()
+        mozi()
 
-def cinema():
+def mozi():
     print("Melyik teremben szeretné nézni a filmet?")
     print("1: VIP terem")
     print("2: 4DX terem")
     print("3: ScreenX")
     a = int(input("Válassza ki a termet: "))
     ticket = int(input("Hány jegyet szeretne vásárolni? "))
-    timing(a)
+    idopont(a)
 
-def timing(a):
-    time1 = {
+def idopont(a):
+    ido1 = {
         "1": "10.00-13.00",
         "2": "13.10-15.10",
         "3": "15.20-17.20",
         "4": "18.30-20.30"
     }
-    time2 = {
+    ido2 = {
         "1": "10.15-13.15",
         "2": "13.25-15.25",
         "3": "15.35-17.35",
         "4": "18.45-20.45"
     }
-    time3 = {
+    ido3 = {
         "1": "10.30-13.30",
         "2": "13.40-15.40",
         "3": "15.50-17.50",
@@ -92,22 +92,22 @@ def timing(a):
     }
     if a == 1:
         print("Melyik időpontban szeretné megnézni?")
-        print(time1)
+        print(ido1)
         t = input("Válassza ki az időpontot:")
-        x = time1[t]
+        x = ido1[t]
         print("Sikeres foglalás, jó szórakozást! ")
     elif a == 2:
         print("Melyik időpontban szeretné megnézni?")
-        print(time2)
+        print(ido2)
         t = input("Válassza ki az időpontot:")
-        x = time2[t]
+        x = ido2[t]
         print("Sikeres foglalás, jó szórakozást!")
     elif a == 3:
         print("Melyik időpontban szeretné megnézni?")
-        print(time3)
+        print(ido3)
         t = input("Válassza ki az időpontot:")
-        x = time3[t]
+        x = ido3[t]
         print("Sikeres foglalás, jó szórakozást!")
     return 0
 
-city()
+varos()
